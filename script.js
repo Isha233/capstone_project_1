e506773dd5271d416bf686b7104c12a71955ac8c
Isha233 <71222055+Isha233@users.noreply.github.com>
2025-01-03 17:20:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+let Fname="isha"
+console.log(Fname)
+ 
+let age=23
+console.log(age)
+ 
+const WaterBottle={
+    colour:"silver",
+    capacity:700,
+    brand:"pigeon",
+    price:400,
+    iswarrantyavailable:true,
+    quantity:1,
+    modelname:"inox hydra 750"
+
+
+
+
+
+}
+console.log(WaterBottle)
+
+const company={
+    name:'HackerRank',
+    founded:2009,
+    Industry:'Software Development',
+    location:'California',
+    TotalEmployees:500,
+    Quote:'change the world to value skills over pedigree',
+    LinkedinFollowers:20000000
+
+}
+console.log(company)
+
+var person={
+    name:'Mark Henry',
+    age:40,
+    gender:'Male',
+    occupation:'Engineer',
+    FacebookIdname:'@mark.Henry.968623',
+    Facebookfriends:645
+
+}
+console.log(person)
+
+let person2={
+    name: 'Sarah Elizabeth',
+    gender:'female',
+    occupation:'blogger',
+    place:'Green Bay WI',
+    instafollowers:4410,
+    Hobby:'travelling',
+    instaposts:265,
+    instafollowing:1812,
+
+}
+console.log(person2)
